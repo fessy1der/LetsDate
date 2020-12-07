0f43580d8e210bae226595aabdc223c1fb5b415b
festus obi <fessy_wonder@yahoo.co.uk>
2020-12-07 14:41:07 +0100

    Add project files.

---
+++ LetsDateAPI/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using LetsDateAPI.Data;
+using LetsDateAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsDateAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public UsersController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users =await _ctx.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user =await _ctx.Users.FindAsync(id);
+            return user;
+        }
+    }
+}
---
+++ LetsDateAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LetsDateAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsDateAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options) { }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
