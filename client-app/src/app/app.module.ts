58ce4cd0b71dca24e7e07d9a03b648545fd56ec0
festus obi <fessy_wonder@yahoo.co.uk>
2021-02-17 12:52:20 +0100

    scafolded extra pages and set up client side routing

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -10,13 +10,21 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MemberDetailsComponent } from './members/member-details/member-details.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    RegisterComponent
+    RegisterComponent,
+    MemberListComponent,
+    MemberDetailsComponent,
+    ListsComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
